522eaebd44aeec87da43f9a832d38a71694e000e
Samuel <aggorcode@yahoo.com>
2026-03-08 14:02:59 -0400

    Initial Commit Structure of the weather app REST API.

---
+++ weather-app/src/App.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from "react"
+// import axios from "axios"
+
+
+function App() {
+
+  const [weatherData, setWeatherData] = useState({});
+  const [location, setLocation] = useState('');
+  
+  return (
+    <div className="weather_app">
+      
+    </div>
+  )
+}
+
+export default App
