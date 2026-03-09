372c8704179ab98e6c54cbb4dc586e62c7d631b1
Samuel <aggorcode@yahoo.com>
2026-03-09 16:00:16 -0400

    Added some containers to design the app layout.

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,17 +1,22 @@
-import React, {useState} from "react"
+// import React, {useState} from "react"
 // import axios from "axios"
 
 
 function App() {
 
-  const [weatherData, setWeatherData] = useState({});
-  const [location, setLocation] = useState('');
+  // const [weatherData, setWeatherData] = useState({});
+  // const [location, setLocation] = useState('');
   
   return (
     <div className="weather_app">
-      <div className="wrapper">
-        <input></input>
+      <div className="banner_wrapper">
+        <div className="logo"></div>
+        <div className="input_container"><input></input></div>
+        <div className="light_dark_mode_button"></div>
       </div>
+      <div className="query_city_weather_display"></div>
+      <div className="hourly_weather_updates"></div>
+      <div className="seven_day_forcast"></div>
     </div>
   )
 }
